bef1dafd39c1cc4314c5ba24a39a6e724129afae
Sarah Guiziou <33069293+sguiz@users.noreply.github.com>
2017-11-10 13:52:51 +0100

    Update and rename recombinase_graph.py to scripts/recombinase_graph.py

--- scripts/recombinase_graph.py
+++ scripts/recombinase_graph.py
@@ -10,11 +10,10 @@ from matplotlib.patheffects import Stroke
 from matplotlib.patches import Polygon, Ellipse, Wedge, Circle, PathPatch
 
 __author__  = 'Bryan Der <bder@mit.edu>, Voigt Lab, MIT\n\
-               Thomas Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT'
+               Thomas Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT\n\
+               Sarah Guiziou <guiziou@cbs.cnrs.fr, Bonnet lab, MIT'
 __license__ = 'MIT'
-__version__ = '1.0'
-
-"""MODIFICATION OF THE CODE BY Sarah GUiziou"""
+__version__ = '2.0'
 
 
 def sbol_recombinase1 (ax, type, num, start, end, prev_end, scale, linewidth, opts):
